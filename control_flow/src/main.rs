e5dd1a07c0a79825b1649627d892a83d248359b4
Raúl Granados <raul.granados12@gmail.com>
2024-07-03 13:59:07 -0600

    Empezando Loops

---
+++ control_flow/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // --- IF ---
+
+    let number = 70;
+
+    if number < 5 {
+        // println!("Se cumplio la condicion");
+    }else {
+        // println!("No se cumplio");
+    }
+
+    // --- LOOPS ---
+
+    // loop {
+    //     println!("again");
+    // }
+
+    
+
+}
--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,16 @@
+#![allow(unused)]
+
 fn another_function(value: i32, unit_label: char){
     println!("Funcion: {value}{unit_label}");
 }
 
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    another_function(5, 'h');
+    // another_function(5, 'h');
+    let x = five();
+
+    println!("Equis: {x}");
 }
