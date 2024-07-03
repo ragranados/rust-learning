0b06dc44a2e94aaba78860c36bfd7a347d812502
ragranados <raul.granados12@gmail.com>
2024-07-02 23:47:47 -0600

    Pruebas

--- pruebas/src/main.rs
+++ pruebas/src/main.rs
@@ -2,7 +2,6 @@
 fn main() {
     // We can be explicit with type
     let mut i32_vec = Vec::<i32>::new(); // turbofish <3
-    
 
     // But look how clever Rust is about determining the type automatically
     let mut float_vec = Vec::new();
@@ -16,6 +15,23 @@ fn main() {
     for word in string_vec.iter() {
         println!("{}", word);
     }
+
+    let signed_unsigned: u32 = 32;
+    println!("Numero: {signed_unsigned}");
+
+    let x: u8 = 255;
+    let y: Option<u8> = x.checked_add(7);
+    println!("{:?}", y); // Output: None
+
+    if y.is_none() {
+        println!("El resultado da overflow");
+    } else {
+        println!("No es overflow");
+    }
+
+    let x: u8 = 100;
+    let y: Option<u8> = x.checked_add(50);
+    println!("{:?}", y); // Output: Some(150)
 
     //Prueba!
 }
