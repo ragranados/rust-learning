27f1721f84dd31e33274a5c9284dbef264b0adaa
Raúl Granados <raul.granados12@gmail.com>
2024-07-04 12:07:34 -0600

    Avances en THe Rust Programming Lenguage

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -5,16 +5,49 @@ fn main() {
 
     if number < 5 {
         // println!("Se cumplio la condicion");
-    }else {
+    } else {
         // println!("No se cumplio");
     }
 
     // --- LOOPS ---
 
-    // loop {
-    //     println!("again");
-    // }
+    loop_en_loop();
 
-    
+}
 
+fn prueba_return() -> i32 {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            return counter * 2;
+        }
+    };
+
+    return 0;
+
+    // println!("The result is {result}");
+}
+
+fn loop_en_loop() {
+    let mut count = 0;
+
+    let x = 'loop_externo: loop {
+
+        println!("Loop Externo: {count}");
+
+        loop {
+            println!("Loop Interno: {count}");
+            count += 1;
+
+            if count == 9 {
+                println!("Saliendo...");
+                break 'loop_externo;
+            }
+        }
+
+
+    };
 }
