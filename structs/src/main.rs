10abfebb61e0b4b5f055e0fa4651059627d8f2d1
Raúl Granados <raul.granados12@gmail.com>
2024-07-08 17:41:04 -0600

    Agreando mas

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,4 @@
-#[allow(unused)]
+#![allow(unused)]
 struct User {
     active: bool,
     username: String,
@@ -9,30 +9,34 @@ struct User {
 struct Color(i32, i32, i32);
 
 fn main() {
-    let mut user = build_user(
-        String::from("myusername"),
-        String::from("myemail@email.com"),
-    );
+    // let mut user = build_user(
+    //     String::from("myusername"),
+    //     String::from("myemail@email.com"),
+    // );
 
-    println!("Valor: {}", user.email);
+    // println!("Valor: {}", user.email);
 
-    user.email = String::from("mynewemail@email.com");
+    // user.email = String::from("mynewemail@email.com");
 
-    println!("Valor: {}", user.email);
+    // println!("Valor: {}", user.email);
 
-    let user2 = User {
-        username: String::from("newusername"),
-        ..user
-    };
+    // let user2 = User {
+    //     username: String::from("newusername"),
+    //     ..user
+    // };
 
-    println!("Valor: {}", user.username);
+    // println!("Valor: {}", user.username);
 
-    let red = Color(0, 0, 0);
+    // let red = Color(0, 0, 0);
 
-    let Color(red, blue, green) = red;
+    // let Color(red, blue, green) = red;
 
-    println!("Red: {red}, Blue: {blue}, Green: {green}");
+    // println!("Red: {red}, Blue: {blue}, Green: {green}");
+
+    // --- Calculo de area de rectangulo ---
 }
+
+
 
 fn build_user(username: String, email: String) -> User {
     User {
