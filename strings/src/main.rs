93d0f7aa4b319cd01deae28e6293f54ece2ed41d
Raúl Granados <raul.granados12@gmail.com>
2024-07-08 12:28:40 -0600

    Pruebas string

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,10 +1,23 @@
+#![allow(unused)]
 fn main() {
     let mut s = String::from("Nueva");
 
-    let mut s2 = s.clone();
+    // let mut s2 = s.clone();
 
-    s.push_str(" String!");
+    // s.push_str(" String!");
 
-    println!("{}", s);
-    println!("{}", s2);
+    // println!("{}", s);
+    // println!("{}", s2);
+
+    first_word(&s);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        println!("Indice: {}, {}", i, item);
+    }
+
+    &s[..]
 }
