c13d1893c53becbe003f3bd062cb198027887246
Raúl Granados <raul.granados12@gmail.com>
2024-07-09 16:08:24 -0600

    Agregando mas pruebas

---
+++ match_learn/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    // println!("Valor en centavos: {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+
+    
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => None,
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("La cora es de {state:?}");
+            25
+        }
+    }
+}
