11ff744d66de1fd5575210b5ff7e1a5a4fcd271e
Raúl Granados <raul.granados12@gmail.com>
2024-07-10 18:06:02 -0600

    Agregando

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -30,12 +30,16 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     
     let mut meal = front_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Francesa");
 
     println!("Desayuno: {}",meal.toast);
+
+    hosting::add_to_waitlist();
     
     // //absoluta
     // crate::front_of_house::hosting::add_to_waitlist();
