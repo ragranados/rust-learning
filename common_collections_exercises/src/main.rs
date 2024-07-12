92bfeb47b85d7d18e753ccf2d76601ac2c498fe0
Raúl Granados <raul.granados12@gmail.com>
2024-07-12 17:54:52 -0600

    Agregando funcion para ejercicio 2

--- common_collections_exercises/src/main.rs
+++ common_collections_exercises/src/main.rs
@@ -7,6 +7,15 @@ fn main() {
 
 }
 
+//Convert strings to pig latin. The first consonant of each word is moved to the end of the word 
+//and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added 
+//to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8
+//encoding!
+
+fn second_exercise() {
+    
+}
+
 //Given a list of integers, use a vector and return the median (when sorted, the value in the
 //middle position) and mode (the value that occurs most often; a hash map will be helpful here)
 //of the list.
