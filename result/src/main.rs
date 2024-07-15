00c01a0a6bd34d463740d2266e68d60fefcc373f
Raúl Granados <raul.granados12@gmail.com>
2024-07-15 17:56:41 -0600

    Subiendo cambios

--- result/src/main.rs
+++ result/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, Read,ErrorKind};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -17,4 +17,20 @@ fn main() {
             }
         },
     };
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
