df11cdedee6cf7247d9034bcd552fa8a0f8cd28d
Raul Granados <raul.granados12@gmail.com>
2024-08-07 10:31:26 -0600

    Refactored code, new Config struct and new method to parse the arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,12 +3,24 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Buscando {query} en {file_path}");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("Con el texto:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
