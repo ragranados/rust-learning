d890bf58fe4b2b08c7eb8d9de844762786362994
Raul Granados <raul.granados12@gmail.com>
2024-08-07 15:16:21 -0600

    Changing error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problema: {err}");
+        process::exit(1);
+    });
 
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
@@ -17,10 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
