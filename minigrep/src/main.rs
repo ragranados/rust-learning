1548ccd937d25be0dabb4342bed6bd211cb27ba7
Raul Granados <raul.granados12@gmail.com>
2024-08-07 22:45:20 -0600

    Adding lib.rs file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,36 +12,10 @@ fn main() {
 
     println!("Buscando {} en {}", config.query, config.file_path);
 
-    let run_result = run(config);
+    let run_result = minigrep::run(config);
 
     if let Err(e) = run_result {
         println!("Error en la aplicacion: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("Con el texto:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
