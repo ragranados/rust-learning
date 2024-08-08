1fd66d7b3c4d1c40edda0d28994237d61da5cdab
Raul Granados <raul.granados12@gmail.com>
2024-08-08 15:50:49 -0600

    Adding eprintln!()

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,15 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|_err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     let run_result = minigrep::run(config);
 
-    if let Err(_e) = run_result {
+    if let Err(e) = run_result {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
