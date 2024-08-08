53e245d718e8abeffa5a690c0740666d91fc2ec5
Raul Granados <raul.granados12@gmail.com>
2024-08-08 11:12:08 -0600

    Finishing minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -21,6 +21,40 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    
+    let result = search(&config.query, &contents);
+
+    for line in result {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    //iterate over each line in contents
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
