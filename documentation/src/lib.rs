184aca32d00f7a8073649cb9a4a443197ffb64c8
Raúl Granados <raul.granados12@gmail.com>
2024-08-13 09:42:51 -0600

    In-code Documentation

---
+++ documentation/src/lib.rs
@@ -0,0 +1,15 @@
+/// Adds to given numbers
+/// 
+/// # Examples
+/// 
+/// ```rust
+/// let n1 = 10;
+/// let n2 = 20;
+/// 
+/// let result = add_numbers(n1, n2);
+/// 
+/// assert_eq!(result, 30);
+/// ```
+pub fn add_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
