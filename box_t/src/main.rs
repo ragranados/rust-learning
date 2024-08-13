293c8f43c209b14bdab32c928f1db67cbc9b3a7c
Raúl Granados <raul.granados12@gmail.com>
2024-08-13 17:48:02 -0600

    Box

--- box_t/src/main.rs
+++ box_t/src/main.rs
@@ -1,4 +1,11 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
+    let list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+
+    let prueba = Box::new(Some(3));
 }
