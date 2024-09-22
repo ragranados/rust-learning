7bf2c9710c538c55187f763ba5fa77de8bf53c5b
Raul Granados <raul.granados12@gmail.com>
2024-09-22 16:26:50 -0600

    Adding my own example with enums

--- state_objects/src/main.rs
+++ state_objects/src/main.rs
@@ -1,7 +1,9 @@
+use state_objects::example_with_enums::Post as PostEnum;
 use state_objects::example_with_state_parttern::Post;
 
-
 fn main() {
+    println!("Example with State Patern: ");
+
     let mut post_one = Post::new();
 
     post_one.add_text("Hi, this is my first post!");
@@ -15,4 +17,24 @@ fn main() {
     post_one.aprove();
 
     println!("Post content: {}", post_one.content());
+
+    println!("Example with Enums: ");
+
+    let mut post_two = PostEnum::new();
+
+    post_two.add_text("Segundo post!");
+
+    println!("Post content: {}", post_two.content());
+
+    post_two.request_review();
+
+    println!("Post content: {}", post_two.content());
+
+    post_two.aprove();
+
+    println!("Post content: {}", post_two.content());
+
+    post_two.request_review();
+
+    // println!("Post content: {}", post_two.content());
 }
