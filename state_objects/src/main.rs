50098c1a67f99ecfcd4100329410c2faa08a4231
Raul Granados <raul.granados12@gmail.com>
2024-09-22 22:21:24 -0600

    Example that utilizes Rust

--- state_objects/src/main.rs
+++ state_objects/src/main.rs
@@ -1,7 +1,14 @@
+use state_objects::changes_to_fit_rust::Post as PostRust;
 use state_objects::example_with_enums::Post as PostEnum;
 use state_objects::example_with_state_parttern::Post;
 
 fn main() {
+    // example_with_state_parttern();
+    // example_with_enums();
+    changes_to_fit_rust();
+}
+
+fn example_with_state_parttern() {
     println!("Example with State Patern: ");
 
     let mut post_one = Post::new();
@@ -34,7 +41,9 @@ fn main() {
 
     println!("Post content: {}", post_one.content());
     post_one.print_state();
+}
 
+fn example_with_enums() {
     //Second example
     println!("Example with Enums: ");
 
@@ -55,4 +64,16 @@ fn main() {
     post_two.request_review();
 
     // println!("Post content: {}", post_two.content());
+}
+
+fn changes_to_fit_rust() {
+    let mut post = PostRust::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.aprove();
+
+    println!("{}", post.content());
 }
