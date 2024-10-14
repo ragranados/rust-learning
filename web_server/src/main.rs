d1ef9c47097482b4593cd273cf6cee9356fe79d9
Raul Granados <raul.granados12@gmail.com>
2024-10-13 22:54:33 -0600

    Refactor

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -21,18 +21,24 @@ fn handle_connection(mut stream: TcpStream) {
     //     println!("{line:#?}")
     // }
 
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // let _http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let html_file = fs::read_to_string("hello.html").unwrap();
-    let length = html_file.len();
+    let (status_line, html_file) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{html_file}");
+    let contents = fs::read_to_string(html_file).unwrap();
+    let length = contents.len();
 
-    stream.write(response.as_bytes()).unwrap();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
